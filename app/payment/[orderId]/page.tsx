07919b1889455c9ca81c8933579c609c664dfe86
bilalm354 <bilalm354@gmail.com>
2024-03-03 13:51:04 +0000

    fix use server bug

--- app/payment/[orderId]/page.tsx
+++ app/payment/[orderId]/page.tsx
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { PRINTIFY_BASE_URL } from "@/app/consts";
 import { PrintifyOrderResponse } from "@/interfaces/PrintifyTypes";
 import { log } from "@/functions/log";
@@ -37,76 +38,19 @@ export default async function PaymentPage({
     ),
   );
 
-  const handleCheckout = async () => {
-    "use server";
-
-    const headersList = headers();
-    const referer = headersList.get("referer") || "";
-    const origin = headersList.get("origin") || "";
-
-    const session = await stripeServerClient.checkout.sessions.create(
-      {
-        customer_email: "test@example.com",
-        shipping_options: [
-          {
-            shipping_rate_data: {
-              // TODO: Figure out dynamically for other shipping options
-              type: "fixed_amount",
-              fixed_amount: {
-                currency: "gbp",
-                amount: total_price,
-              },
-              display_name: "Standard",
-              delivery_estimate: {
-                minimum: {
-                  unit: "business_day",
-                  value: 3,
-                },
-                maximum: {
-                  unit: "business_day",
-                  value: 5,
-                },
-              },
-            },
-          },
-        ],
-        line_items: [
-          {
-            price_data: {
-              currency: "gbp",
-              product_data: {
-                name: "T-shirt", // TODO: Figure out dynamically
-              },
-              unit_amount_decimal: total_price.toString(),
-            },
-            quantity: 1,
-          },
-        ],
-        mode: "payment",
-        success_url: `${origin}/payment/success`,
-        cancel_url: referer,
-        automatic_tax: {
-          enabled: true,
-          liability: {
-            type: "self",
-          },
-        },
-      },
-      {},
-    );
-    redirect(session!.url!);
-  };
-
   return (
     <>
       <h1 className="text-xl">Order Details</h1>
       <p>Total Price: {total_price}</p>
       <p>Total Shipping: {total_shipping}</p>
       <p>Total Tax: {total_tax}</p>
+      <p>Total: {total_price + total_shipping + total_tax}</p>
+
       <form
         action={handleCheckout}
         className="mt-8 block w-1/5 rounded-md py-2 text-center text-sm font-semibold text-white ring-2 bg-blue-500 hover:bg-blue-800 hover:ring-0"
       >
+        <input type="hidden" name="total_price" value={total_price} />
         <button type="submit">Proceed to checkout</button>
       </form>
       <h1 className="text-xl pt-8">Your Basket</h1>
@@ -128,4 +72,66 @@ async function getOrderDetails(orderId: string) {
   const orderDetails = (await response.json()) as PrintifyOrderResponse;
   log({ orderDetails });
   return orderDetails;
+}
+
+async function handleCheckout(formData: FormData) {
+  "use server";
+  const rawFormData = Object.fromEntries(formData.entries());
+  const schema = z.object({
+    total_price: z.string(),
+  });
+  const parsedFormData = schema.parse(rawFormData);
+  const total_price = Number(parsedFormData.total_price); // will one hundo percent cause a bug
+  const headersList = headers();
+  const referer = headersList.get("referer") || "";
+  const origin = headersList.get("origin") || "";
+
+  const session = await stripeServerClient.checkout.sessions.create({
+    customer_email: "test@example.com",
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          // TODO: Figure out dynamically for other shipping options
+          type: "fixed_amount",
+          fixed_amount: {
+            currency: "gbp",
+            amount: total_price,
+          },
+          display_name: "Standard",
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 3,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 5,
+            },
+          },
+        },
+      },
+    ],
+    line_items: [
+      {
+        price_data: {
+          currency: "gbp",
+          product_data: {
+            name: "T-shirt", // TODO: Figure out dynamically
+          },
+          unit_amount_decimal: total_price.toString(),
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${origin}/payment/success`,
+    cancel_url: referer,
+    automatic_tax: {
+      enabled: true,
+      liability: {
+        type: "self",
+      },
+    },
+  });
+  redirect(session!.url!);
 }

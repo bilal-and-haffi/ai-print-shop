a154091a3384a7caa71723247c55dbb0eebb2f01
Haffi Mazhar <haffimazhar96@gmail.com>
2024-03-07 18:48:44 +0000

    trry with replicate lib

--- lib/images/replicate.ts
+++ lib/images/replicate.ts
@@ -1,72 +1,40 @@
 import { log } from "console";
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_KEY,
+});
 
 export const revalidate = 0;
 
-async function createReplicateImageJob(prompt: string) {
-  return await fetch(
-    "https://api.replicate.com/v1/predictions",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Token ${process.env.REPLICATE_API_KEY}`,
-      },
-      body: JSON.stringify({
-        "version": "610dddf033f10431b1b55f24510b6009fcba23017ee551a1b9afbc4eec79e29c",
-        "input": {
-          "width": 1024,
-          "height": 1024,
-          "prompt": `IMAGE PROMPT: ${prompt}`,
-          "refine": "expert_ensemble_refiner",
-          "scheduler": "KarrasDPM",
-          "num_outputs": 1,
-          "guidance_scale": 7.5,
-          "high_noise_frac": 0.8,
-          "prompt_strength": 0.8,
-          "num_inference_steps": 50
-        }
-      })
-    })
-}
-
-async function getReplicatePrediction(getPredicationUrl: string) {
-  return await fetch(
-    getPredicationUrl,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Token ${process.env.REPLICATE_API_KEY}`,
-      },
-    })
-}
 
 export const generateReplicateImageUrl: (prompt: string) => Promise<string> = async (
   prompt: string,
 ) => {
   try {
-    log("Generating image with prompt", prompt);
-    const prediction = await createReplicateImageJob(prompt);
-    const createJobResponse = await prediction.json();
-    let getPredictionStatusUrl = createJobResponse.urls.get;
-    log("Prediction status url:", getPredictionStatusUrl);
-    let predictionUrl = "";
-    let predictionStatus = "starting";
-
-    while (predictionStatus !== "succeeded") {
-      const predictionResult = await getReplicatePrediction(getPredictionStatusUrl);
-      const predictionResultJson = await predictionResult.json();
-      predictionStatus = predictionResultJson.status;
-      log("Prediction status:", predictionStatus);
-      if (predictionStatus === "succeeded") {
-        predictionUrl = predictionResultJson.output[0];
-      } else {
-        // Wait for 5 seconds before polling again
-        await new Promise(resolve => setTimeout(resolve, 5000));
+    log("Generating image with prompt:", prompt);
+    const output = await replicate.run(
+      "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+      {
+        input: {
+          width: 768,
+          height: 768,
+          prompt: `IMAGE PROMPT: ${prompt}`,
+          refine: "expert_ensemble_refiner",
+          scheduler: "K_EULER",
+          lora_scale: 0.6,
+          num_outputs: 1,
+          guidance_scale: 7.5,
+          apply_watermark: false,
+          high_noise_frac: 0.8,
+          negative_prompt: "",
+          prompt_strength: 0.8,
+          num_inference_steps: 25
+        }
       }
-    }
-
-    return predictionUrl;
+    ) as string[];
+    log("output:", output);
+    return output[0];
   } catch (error) {
     console.error("Error generating image:", error);
     throw new Error("Error generating image");

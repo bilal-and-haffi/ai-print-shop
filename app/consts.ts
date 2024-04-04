5640f52172cb0404de3c00c3c87141b2079796ca
bilalm354 <bilalm354@gmail.com>
2024-04-04 03:02:18 +0100

    remove unused sample address const;

--- app/consts.ts
+++ app/consts.ts
@@ -1,13 +1,1 @@
 export const PRINTIFY_BASE_URL = "https://api.printify.com";
-export const sampleAddressTo = {
-  first_name: "John",
-  last_name: "Smith",
-  email: "example@msn.com",
-  phone: "0574 69 21 90",
-  country: "BE",
-  region: "",
-  address1: "ExampleBaan 121",
-  address2: "45",
-  city: "Retie",
-  zip: "2470",
-};

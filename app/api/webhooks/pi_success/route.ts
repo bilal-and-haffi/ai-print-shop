335cd67464664df20f796eb8ced5306a81802a2f
Haffi Mazhar <haffimazhar96@gmail.com>
2024-04-06 02:45:16 +0100

    feat: use stripe checkout shipping

---
+++ app/api/webhooks/pi_success/route.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { log } from '@/utils/log';
+import { createPrintifyOrderForExistingProduct } from '@/lib/printify/service';
+import { LineItemBase } from '@/interfaces/PrintifyTypes';
+
+export async function POST(request: NextRequest) {
+  const req = await request.json();
+
+  const eventData = req.data;
+
+  const shipping = eventData.object.shipping;
+
+  const metaData = eventData.object.metadata;
+
+  console.log({ shipping, metaData });
+
+
+  const line_items: LineItemBase[] = [
+    {
+      product_id: metaData.product_id,
+      variant_id: 12124, // make me dynamic
+      quantity: 1,
+    },
+  ];
+
+  const shipping_method = 1; // make me dynamic
+
+  const orderId = await createPrintifyOrderForExistingProduct(
+    line_items,
+    shipping_method,
+    {
+      first_name: shipping.name.split(" ")[0],
+      last_name: shipping.name.split(" ")[1],
+      email: shipping.email,
+      phone: shipping.phone,
+      country: shipping.address.country,
+      region: shipping.address.state,
+      address1: shipping.address.line1,
+      address2: shipping.address.line2,
+      city: shipping.address.city,
+      zip: shipping.address.postal_code,
+    }
+  );
+
+  log({ orderId });
+
+  return NextResponse.json({ message: "success" });
+}
---
+++ app/checkout/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { createCheckoutSession } from '@/lib/stripe/service';
+import { T_SHIRT_PRICE_IN_GBP } from '@/app/data/consts';
+import { log } from '@/utils/log';
+
+export async function POST(request: NextRequest) {
+  const req = await request.json();
+
+  const totalShipping = Number(0);
+  const totalStripePrice = Number(T_SHIRT_PRICE_IN_GBP) * 100;
+  const headersList = request.headers;
+  const referer = headersList.get("referer") || "";
+  const origin = headersList.get("origin") || "";
+
+  const productId = req.product_id;
+
+  log({ productId });
+
+  const session = await createCheckoutSession({
+    referer,
+    origin,
+    totalStripePrice,
+    totalShipping,
+    orderTitle: req.order_title,
+    orderVariantLabel: req.order_variant_label,
+    orderPreview: req.order_preview,
+    productId: req.product_id,
+  });
+
+  return NextResponse.json({ message: "success", url: session!.url! });
+}

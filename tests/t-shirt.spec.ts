45a7b4b62e3ab6e158042646df12efdd5564f3cc
bilalm354 <bilalm354@gmail.com>
2024-04-08 01:17:48 +0100

    add screenshots

--- tests/t-shirt.spec.ts
+++ tests/t-shirt.spec.ts
@@ -7,10 +7,13 @@ test("has title", async ({ page }) => {
 
 test("should allow user to buy a t shirt", async ({ page }) => {
     await page.goto("/");
+    await page.screenshot({ path: "tests/screenshots/homepage.png" });
     // in test env we will already have "test" in the input field and that is mocked
     const generateButton = await page.getByText("Generate");
     await generateButton.click();
     await expect(page).toHaveURL(/\/tshirt/);
+
+    await page.screenshot({ path: "tests/screenshots/tshirt-page.png" });
 
     const tShirtButton = await page.getByText("T Shirt");
     await expect(tShirtButton).toBeVisible();

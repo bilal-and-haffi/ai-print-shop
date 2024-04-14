9ff0ff54f3dbb573ff6741fd423f544a6f32ca76
bilalm354 <bilalm354@gmail.com>
2024-04-14 14:10:16 +0100

    add openai model to generate new image with same prompt - hack fix

--- components/GenerateImageLinks.tsx
+++ components/GenerateImageLinks.tsx
@@ -13,7 +13,7 @@ export const GenerateImageLinks = ({ prompt }: { prompt: string }) => {
                 </Link>
             </div>
             <div id="linkContainer" className="w-full self-center">
-                <a href={`/image/${prompt}`}>
+                <a href={`/image/${prompt}?model=openai`}>
                     {/* Using <Link> instead of <a> here caused a bug where this wouldn't work for many seconds after page load. */}
                     <Button className="focus:shadow-outline w-full rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700 focus:outline-none">
                         Generate new image with same prompt

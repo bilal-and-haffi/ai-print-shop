2897944b8c5ec091e9d0fc81568a1404d9e6df70
bilalm354 <bilalm354@gmail.com>
2024-07-05 08:49:08 +0100

    change titles and email address

--- tests/t-shirt.spec.ts
+++ tests/t-shirt.spec.ts
@@ -2,12 +2,12 @@ import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
     await page.goto("/");
-    await expect(page).toHaveTitle(/AI Personalised Gift Shop/);
+    await expect(page).toHaveTitle(/AI Print Shop/);
 });
 
 test("shows email address", async ({ page }) => {
     await page.goto("/");
-    const emailButton = await page.getByText("ai-personalised-gifts@mail.com");
+    const emailButton = await page.getByText("ai-print-shop@mail.com");
     await expect(emailButton).toBeVisible();
 });
 

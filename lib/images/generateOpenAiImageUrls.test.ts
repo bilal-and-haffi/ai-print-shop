a3045cf5e6e03e5f794b88aa2a946663af46c591
bilalm354 <bilalm354@gmail.com>
2024-07-11 08:52:03 +0100

    added test to check that the urls returns from multiple images are different

--- lib/images/generateOpenAiImageUrls.test.ts
+++ lib/images/generateOpenAiImageUrls.test.ts
@@ -2,22 +2,35 @@ import { generateOpenAiImageUrls } from "./generateOpenAiImageUrl";
 
 describe("generateOpenAiImageUrls", () => {
     // test to ensure that the test prompts work as expected too
-    it("returns 2 image urls when it is passed 2 images with the test prompt", async () => {
-        const urls = await generateOpenAiImageUrls({
-            prompt: "test prompt",
-            numberOfImages: 2,
-            style: "vivid",
+    describe("with test prompt", () => {
+        it("returns 2 image urls when it is passed 2 images with the test prompt", async () => {
+            const urls = await generateOpenAiImageUrls({
+                prompt: "test prompt",
+                numberOfImages: 2,
+                style: "vivid",
+            });
+            expect(urls.length).toBe(2);
         });
-        expect(urls.length).toBe(2);
     });
 
-    // skipped because uses real images and don't want the costs
-    it.skip("returns 2 image urls when it is passed 2 images", async () => {
-        const urls = await generateOpenAiImageUrls({
-            prompt: "flying fish",
-            numberOfImages: 2,
-            style: "vivid",
+    describe.skip("with real prompt and api request", () => {
+        // skipped because uses real images and don't want the costs
+        let urls: string[] = [];
+        beforeAll(async () => {
+            urls = await generateOpenAiImageUrls({
+                prompt: "flying fish",
+                numberOfImages: 2,
+                style: "vivid",
+            });
+        }, 30000);
+
+        it("returns 2 image urls when it is passed 2 images", async () => {
+            expect(urls.length).toBe(2);
         });
-        expect(urls.length).toBe(2);
-    }, 30000);
+
+        it("returns different urls", () => {
+            const areBothUrlsTheSame = urls[0] === urls[1];
+            expect(areBothUrlsTheSame).toBeFalsy();
+        });
+    });
 });

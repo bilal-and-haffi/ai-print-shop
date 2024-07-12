e9661f762d9bc7139e9bcbe920a6417e1683c03a
bilalm354 <bilalm354@gmail.com>
2024-07-12 09:27:35 +0100

    added ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { CardFooter } from "@/components/ui/card";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,21 +26,30 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" className="h-full overflow-x-hidden">
-            <body
-                className={`${inter.className} flex h-full flex-col bg-black text-white`}
-            >
+        <html
+            lang="en"
+            className="h-full overflow-x-hidden"
+            suppressHydrationWarning
+        >
+            <body className={`${inter.className} flex h-full flex-col`}>
                 <main className="flex flex-1 flex-col items-center space-y-4 py-8 lg:justify-center lg:py-16">
                     <div id="heading">
                         <h1 className="text-2xl font-bold">AI Print Shop</h1>
                     </div>
-                    {children}
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                    >
+                        {children}
+                    </ThemeProvider>
                 </main>
                 <footer className="mt-auto w-full">
                     <CardFooter className="p-0">
                         <Button
                             asChild
-                            className="focus:shadow-outline w-full rounded-none px-4 py-2 text-white focus:outline-none"
+                            className="focus:shadow-outline w-full rounded-none focus:outline-none"
                         >
                             <Link href="mailto:ai-print-shop@mail.com">
                                 Contact us at ai-print-shop@mail.com

7ad8cb1973f25dc41f9db52ca0e494a8a576e7fe
Bilal Minhas <53113053+Bilalm354@users.noreply.github.com>
2024-07-13 11:49:45 +0100

    Revert "improved loading screens"
    This reverts commit 09c99397c1158bffa9c4bfdf14d9e40f4ce9eda6.

--- app/product/[printifyImageId]/loading.tsx
+++ app/product/[printifyImageId]/loading.tsx
@@ -1,8 +1,9 @@
-import { ProgressBar } from "@/components/loading/ProgressBar";
+import { ProgressBar } from "@/components/ProgressBar";
 
 export default function Loading() {
     return (
         <>
+            <p>Loading</p>
             <ProgressBar estimatedTimeInMs={2000} />
             <p>Your products are being created!</p>
         </>

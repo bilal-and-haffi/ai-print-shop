f735c797e9543b52e77b4e6db74d8e3e9ce2a9d0
bilalm354 <bilalm354@gmail.com>
2024-07-13 23:38:11 +0100

    refactor create product requests - t shirt and mug working but hoodie broken

---
+++ lib/printify/products/products.ts
@@ -0,0 +1,29 @@
+interface PrintifyProducts {
+    name: string;
+    enabled: boolean;
+    blueprintId: number;
+    printProviderId: number;
+}
+
+const PRINT_CLEVER_PRINT_PROVIDED_ID = 72;
+
+export const products = {
+    tShirt: {
+        name: "Unisex Heavy T Shirt Gildan 5000",
+        enabled: true,
+        blueprintId: 6,
+        printProviderId: PRINT_CLEVER_PRINT_PROVIDED_ID,
+    },
+    hoodie: {
+        name: "Unisex Heavy Blend Hooded Sweatshirt",
+        enabled: true,
+        blueprintId: 6,
+        printProviderId: PRINT_CLEVER_PRINT_PROVIDED_ID,
+    },
+    mug: {
+        name: "Mug",
+        enabled: false,
+        printProviderId: PRINT_CLEVER_PRINT_PROVIDED_ID,
+        blueprintId: 1302,
+    },
+};

dc9473e618ba6842b249b952270866829b987116
Bilal Minhas <53113053+Bilalm354@users.noreply.github.com>
2024-07-13 11:49:45 +0100

    Revert "improve loading screens"
    This reverts commit 1346818ea1715bcbc38a1119a2b67940a455a251.

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import { PageLoadingSpinner } from "@/components/PageLoadingSpinner";
+
+export default function Loading() {
+    return (
+        <>
+            <p>Loading</p>
+            <PageLoadingSpinner />
+        </>
+    );
+}

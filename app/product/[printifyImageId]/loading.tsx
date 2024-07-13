ee273d31d794d430a99982ddc2275e3d73e7d8f6
bilalm354 <bilalm354@gmail.com>
2024-07-13 10:51:35 +0100

    made product page loading consisten with image loading

--- app/product/[printifyImageId]/loading.tsx
+++ app/product/[printifyImageId]/loading.tsx
@@ -3,6 +3,7 @@ import { ProgressBar } from "@/components/loading/ProgressBar";
 export default function Loading() {
     return (
         <>
+            <p>Please wait</p>
             <ProgressBar estimatedTimeInMs={2000} />
             <p>Your products are being created!</p>
         </>

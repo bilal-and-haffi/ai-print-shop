9c175a1b6013f1bfb0d7105d6adfa3e16fa9bf5b
Bilal Minhas <53113053+Bilalm354@users.noreply.github.com>
2024-07-13 11:49:45 +0100

    Revert "made product page loading consisten with image loading"
    This reverts commit ee273d31d794d430a99982ddc2275e3d73e7d8f6.

--- app/product/[printifyImageId]/loading.tsx
+++ app/product/[printifyImageId]/loading.tsx
@@ -3,7 +3,6 @@ import { ProgressBar } from "@/components/loading/ProgressBar";
 export default function Loading() {
     return (
         <>
-            <p>Please wait</p>
             <ProgressBar estimatedTimeInMs={2000} />
             <p>Your products are being created!</p>
         </>

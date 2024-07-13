c83c4df02af63babba5761e3869343c2bb1678f7
bilalm354 <bilalm354@gmail.com>
2024-07-13 14:41:32 +0100

    update loading times for progress bars

--- app/image/[prompt]/loading.tsx
+++ app/image/[prompt]/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
     return (
         <>
             <p>Please wait</p>
-            <ProgressBar estimatedTimeInMs={12000} />
+            <ProgressBar estimatedTimeInMs={20000} />
             <p>Your image is being generated!</p>
             {/* Add which model they are using */}
         </>

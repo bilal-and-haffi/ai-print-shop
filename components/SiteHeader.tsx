06f9950e734f31414db497adf5b1d3f08811d88f
bilalm354 <bilalm354@gmail.com>
2024-07-16 20:02:34 +0100

    replace words with logos

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -6,26 +6,22 @@ import { Bot, MessageSquareMore } from "lucide-react";
 export function SiteHeader() {
     return (
         <header className="sticky top-0 z-50 w-full  border-b-white bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-            <div className="container mx-auto flex max-w-screen-2xl items-center justify-between p-2 align-middle">
+            <div className="container mx-auto flex max-w-screen-lg items-center justify-between p-2 align-middle">
+                <Link className="hover:text-zinc-200" href="/support">
+                    <MessageSquareMore />
+                </Link>
+                {/* <Link
+                    className="hidden hover:text-zinc-200 md:block"
+                    href="/support"
+                >
+                    Support
+                </Link> */}
+
                 <Link className="flex space-x-2" href="/">
-                    <Bot />
+                    {/* <Bot /> */}
                     <span className="font-bold">ai-print-shop</span>
                 </Link>
-                <div className="flex space-x-8">
-                    <TipsSheet />
-                    <Link
-                        className="hidden hover:text-zinc-200 md:block"
-                        href="/support"
-                    >
-                        Support
-                    </Link>
-                    <Link
-                        className="hover:text-zinc-200 md:hidden"
-                        href="/support"
-                    >
-                        <MessageSquareMore />
-                    </Link>
-                </div>
+                <TipsSheet />
             </div>
             <Separator />
         </header>

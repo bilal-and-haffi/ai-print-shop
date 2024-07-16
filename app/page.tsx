73ea891ce31d8d221e42838bae81f44808eb4c7f
bilalm354 <bilalm354@gmail.com>
2024-07-16 20:57:07 +0100

    replace to have only one card on home page

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,10 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="flex w-full flex-col space-y-4 md:space-y-8">
-            <Card className="p-4 text-sm">
+            <Card className="p-4">
                 <p>
                     Create personalized custom clothes with AI-generated images!
                 </p>
-            </Card>
-            <Card className="p-4 text-sm">
-                <p>Easy to order, print-on-demand T-shirts and apparel.</p>
             </Card>
             <TextAreaAndButton />
         </div>

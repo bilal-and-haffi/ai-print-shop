340ce586b69c72b148cd2740e33c4bd1bba7d3f9
Bilal Minhas <53113053+Bilalm354@users.noreply.github.com>
2024-07-16 21:32:00 +0100

    Update SiteHeader.tsx

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -6,7 +6,7 @@ import { Bot, MessageSquareMore } from "lucide-react";
 export function SiteHeader() {
     return (
         <header className="sticky top-0 z-50 w-full  border-b-white bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-            <div className="container mx-auto flex max-w-screen-lg items-center justify-between p-4 text-xl align-middle md:p-0 md:py-4">
+            <div className="container mx-auto flex max-w-screen-lg items-center justify-between p-4 align-middle md:p-0 md:py-4">
                 <Link className="hover:text-zinc-200" href="/support">
                     <MessageSquareMore />
                 </Link>

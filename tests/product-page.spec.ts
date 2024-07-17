ea02ae2d2db176349f6548449c76f550bb9ab979
bilalm354 <bilalm354@gmail.com>
2024-07-17 16:14:55 +0100

    removed skipped tests;

--- tests/product-page.spec.ts
+++ tests/product-page.spec.ts
@@ -16,27 +16,3 @@ test("should not have invalid model text", async ({ page }) => {
 test('should have url "/product"', async ({ page }) => {
     await expect(page).toHaveURL(/\/product/, { timeout: 30000 });
 });
-
-test.skip("should allow user to switch between products", async ({ page }) => {
-    // get buttons
-    const tShirtButton = page.getByRole("button", { name: "T Shirt" });
-    const hoodieButton = page.getByRole("button", { name: "Hoodie" });
-    const mugButton = page.getByRole("button", { name: "Mug" });
-
-    // Check if the product buttons are visible
-    await expect(tShirtButton).toBeVisible({ timeout: 10000 });
-    await expect(hoodieButton).toBeVisible({ timeout: 10000 });
-    await expect(mugButton).toBeVisible({ timeout: 10000 });
-
-    // check if can go to hoodie product
-    await hoodieButton.click();
-    await expect(tShirtButton).not.toHaveClass(/ring-white/);
-    await expect(hoodieButton).toHaveClass(/ring-white/);
-    await expect(mugButton).not.toHaveClass(/ring-white/);
-
-    // check if can go to mug product
-    await mugButton.click();
-    await expect(tShirtButton).not.toHaveClass(/ring-white/);
-    await expect(hoodieButton).not.toHaveClass(/ring-white/);
-    await expect(mugButton).toHaveClass(/ring-white/);
-});

870a5247346a85fb6e559e33dbdacc324b4368c5
bilalm354 <bilalm354@gmail.com>
2024-07-17 17:59:03 +0100

    header styling

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -6,14 +6,23 @@ import { Bot, MessageSquareMore } from "lucide-react";
 export function SiteHeader() {
     return (
         <header className="sticky top-0 z-50 w-full  border-b-white bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-            <div className="container mx-auto flex max-w-screen-md items-center justify-between p-4 align-middle md:p-0 md:py-4">
+            <div className="container mx-auto flex max-w-screen-2xl items-center justify-between p-4 align-middle md:p-0 md:py-4">
                 <Link className="flex space-x-2" href="/">
                     <Bot />
                     <span>AI Print Shop</span>
                 </Link>
-                <div className="flex space-x-4">
-                    <Link className="hover:text-zinc-200" href="/support">
+                <div className="flex space-x-4 md:space-x-16">
+                    <Link
+                        className="hover:text-zinc-200 md:hidden"
+                        href="/support"
+                    >
                         <MessageSquareMore />
+                    </Link>
+                    <Link
+                        className="hidden hover:text-zinc-200 md:block"
+                        href="/support"
+                    >
+                        Support
                     </Link>
 
                     <TipsSheet />

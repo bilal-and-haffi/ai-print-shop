b9bdb1e834a7d81feb17db5c538111f2431e175f
bilalm354 <bilalm354@gmail.com>
2024-07-19 16:16:00 +0100

    change currency api to one with 5000 rate limit instead of 300

--- lib/currency/convertUSDToGBP.ts
+++ lib/currency/convertUSDToGBP.ts
@@ -2,11 +2,11 @@
 import { envServer } from "@/lib/env/server";
 
 export async function convertUSDToGBP(usd: number) {
-    const endpoint = `https://api.currencyapi.com/v3/latest?apikey=${envServer.CURRENCY_API_KEY}&currencies=USD&base_currency=GBP`;
+    const endpoint = `https://api.freecurrencyapi.com/v1/latest?apikey=${envServer.FREE_CURRENCY_API_KEY}&currencies=USD&base_currency=GBP`;
     const response = await fetch(endpoint);
     const data = await response.json();
     const { USD } = data.data;
-    const gbpInUsd = USD.value;
+    const gbpInUsd = USD;
     const gbp = usd / gbpInUsd;
 
     return gbp;

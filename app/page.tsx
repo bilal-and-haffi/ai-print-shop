c47a8c6015b96a7d7a2816d6919357cc98c4de91
bilalm354 <bilalm354@gmail.com>
2024-07-21 00:28:27 +0100

    add hoodie example card

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
-import { Card } from "@/components/ui/card";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 import { ImageGenerationForm } from "../components/ImageGenerationForm";
 import { InstructionsDialog } from "@/components/InstructionsDialog";
+import Image from "next/image";
 
 export default function Home() {
     return (
@@ -11,9 +18,45 @@ export default function Home() {
                     on demand!
                 </p>
             </Card>
+            <ExampleCard
+                productType="Hoodie"
+                prompt="An astronaut playing on an old arcade game in space"
+                imageSrc="astronaut-playing-arcade-hoodie.jpeg"
+                imageAlt="astronaut"
+            />
 
             <ImageGenerationForm />
             <InstructionsDialog />
         </div>
+    );
+}
+
+function ExampleCard({
+    productType,
+    prompt,
+    imageSrc,
+    imageAlt,
+}: {
+    productType: string;
+    prompt: string;
+    imageSrc: string;
+    imageAlt: string;
+}) {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>{productType}</CardTitle>
+                <CardDescription>Prompt: {prompt}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Image
+                    alt={imageAlt}
+                    src={imageSrc}
+                    width={1000}
+                    height={1000}
+                    priority
+                />
+            </CardContent>
+        </Card>
     );
 }

a2234425a0f38c6a65035e414d46ba362c3ed96c
Bilal Minhas <53113053+Bilalm354@users.noreply.github.com>
2024-07-21 17:30:46 +0100

    Update ImageGenerationForm.tsx

--- components/ImageGenerationForm.tsx
+++ components/ImageGenerationForm.tsx
@@ -108,7 +108,7 @@ export function ImageGenerationForm() {
     });
 
     return (
-        <div className="w-full flex flex-col items-center">
+        <div className="flex flex-col w-full items-center gap-4">
             <h1 className="text-2xl">Enter your idea</h1>
             {showConfirmationDialog && (
                 <PromptConfirmationDialog

281e71b144aaf655764e85de4eafabdb5c8ec2ff
bilalm354 <bilalm354@gmail.com>
2024-07-21 13:28:56 +0100

    styling

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -3,7 +3,7 @@ import { InstructionsDialog } from "@/components/InstructionsDialog";
 
 export default async function CreatePage() {
     return (
-        <div className="flex w-full flex-col gap-4">
+        <div className="flex w-full flex-col gap-4 md:w-2/3">
             <ImageGenerationForm />
             <InstructionsDialog />
         </div>

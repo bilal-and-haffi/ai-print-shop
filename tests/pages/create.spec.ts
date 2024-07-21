facdc3e97c00c6427e93b4e6d5ca2ab73a6dd8a7
bilalm354 <bilalm354@gmail.com>
2024-07-21 02:20:26 +0100

    make test pass

--- tests/pages/create.spec.ts
+++ tests/pages/create.spec.ts
@@ -9,9 +9,12 @@ test("User can select None for location and style and this should mean that the 
 }) => {
     await page.getByPlaceholder("Example: An astronaut playing").click();
     await page.getByPlaceholder("Example: An astronaut playing").fill("bird");
-    await page.locator('[id="\\:Raaujkq\\:-form-item"]').click();
+    await page
+        .locator("button")
+        .filter({ hasText: "Style: Photograph" })
+        .click();
     await page.getByLabel("Style: None").click();
-    await page.locator('[id="\\:Reaujkq\\:-form-item"]').click();
+    await page.locator("button").filter({ hasText: "Location: Space" }).click();
     await page.getByLabel("Location: None").click();
     await page.getByTestId("Generate Image Button").click();
     await page.waitForURL(/product/);

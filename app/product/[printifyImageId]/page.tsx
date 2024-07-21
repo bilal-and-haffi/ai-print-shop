eb5fc8245b4577b6bc27ed1a92f127ac9ab54688
bilalm354 <bilalm354@gmail.com>
2024-07-21 20:12:59 +0100

    extract getProductAndVariants

--- app/product/[printifyImageId]/page.tsx
+++ app/product/[printifyImageId]/page.tsx
@@ -1,85 +1,19 @@
 import { Products } from "@/components/Products";
-import { Variant } from "@/interfaces/Printify/Variant";
-import { createPrintifyProduct } from "@/lib/printify/createPrintifyProduct";
-import { fetchProductVariants } from "@/lib/printify/fetchProductVariants";
-import { products } from "@/lib/printify/productsData";
-import { ProductType } from "@/types/ProductType";
+import { getProductsAndVariants } from "../../../lib/printify/getProductsAndVariants";
 
 export default async function ProductPage({
     params,
+    searchParams,
 }: {
     params: { printifyImageId: string };
+    searchParams: { country: string };
 }) {
     const { printifyImageId } = params;
-    console.log({ printifyImageId });
-
-    const [
-        tShirtProduct,
-        hoodieProduct,
-        mugProduct,
-        tShirtVariants,
-        hoodieVariants,
-        mugVariants,
-    ] = await Promise.all([
-        createPrintifyProduct({
-            printifyImageId,
-            printProviderId: products.tShirt.printProviderId,
-            blueprintId: products.tShirt.blueprintId,
-        }),
-        createPrintifyProduct({
-            printifyImageId,
-            printProviderId: products.hoodie.printProviderId,
-            blueprintId: products.hoodie.blueprintId,
-        }),
-        createPrintifyProduct({
-            printifyImageId,
-            printProviderId: products.mug.printProviderId,
-            blueprintId: products.mug.blueprintId,
-        }),
-        // The variants below are fetched because they reflect what is in stock whereas the ProductVariant on the product respones to not reflect what is in stock
-        fetchProductVariants(
-            products.tShirt.blueprintId,
-            products.tShirt.printProviderId,
-        ),
-        fetchProductVariants(
-            products.hoodie.blueprintId,
-            products.hoodie.printProviderId,
-        ),
-        fetchProductVariants(
-            products.mug.blueprintId,
-            products.mug.printProviderId,
-        ),
-    ]);
-
-    const productAndVariants: Map<
-        ProductType,
-        {
-            product: any;
-            variants: Variant[];
-        }
-    > = new Map([
-        [
-            ProductType.TShirt,
-            {
-                product: tShirtProduct,
-                variants: tShirtVariants,
-            },
-        ],
-        [
-            ProductType.Hoodie,
-            {
-                product: hoodieProduct,
-                variants: hoodieVariants,
-            },
-        ],
-        [
-            ProductType.Mug,
-            {
-                product: mugProduct,
-                variants: mugVariants,
-            },
-        ],
-    ]);
+    const { country } = searchParams;
+    const productAndVariants = await getProductsAndVariants({
+        printifyImageId,
+        country,
+    });
 
     return (
         <div className="flex flex-col gap-4 md:w-1/2">
---
+++ lib/printify/getProductsAndVariants.ts
@@ -0,0 +1,83 @@
+import { Variant } from "@/interfaces/Printify/Variant";
+import { createPrintifyProduct } from "@/lib/printify/createPrintifyProduct";
+import { fetchProductVariants } from "@/lib/printify/fetchProductVariants";
+import { products } from "@/lib/printify/productsData";
+import { ProductType } from "@/types/ProductType";
+
+export async function getProductsAndVariants({
+    printifyImageId,
+    country,
+}: {
+    printifyImageId: string;
+    country?: string;
+}) {
+    const [
+        tShirtProduct,
+        hoodieProduct,
+        mugProduct,
+        tShirtVariants,
+        hoodieVariants,
+        mugVariants,
+    ] = await Promise.all([
+        createPrintifyProduct({
+            printifyImageId,
+            printProviderId: products.tShirt.printProviderId,
+            blueprintId: products.tShirt.blueprintId,
+        }),
+        createPrintifyProduct({
+            printifyImageId,
+            printProviderId: products.hoodie.printProviderId,
+            blueprintId: products.hoodie.blueprintId,
+        }),
+        createPrintifyProduct({
+            printifyImageId,
+            printProviderId: products.mug.printProviderId,
+            blueprintId: products.mug.blueprintId,
+        }),
+        // The variants below are fetched because they reflect what is in stock whereas the ProductVariant on the product respones to not reflect what is in stock
+        fetchProductVariants(
+            products.tShirt.blueprintId,
+            products.tShirt.printProviderId,
+        ),
+        fetchProductVariants(
+            products.hoodie.blueprintId,
+            products.hoodie.printProviderId,
+        ),
+        fetchProductVariants(
+            products.mug.blueprintId,
+            products.mug.printProviderId,
+        ),
+    ]);
+
+    const productAndVariants: Map<
+        ProductType,
+        {
+            product: any;
+            variants: Variant[];
+        }
+    > = new Map([
+        [
+            ProductType.TShirt,
+            {
+                product: tShirtProduct,
+                variants: tShirtVariants,
+            },
+        ],
+        [
+            ProductType.Hoodie,
+            {
+                product: hoodieProduct,
+                variants: hoodieVariants,
+            },
+        ],
+        [
+            ProductType.Mug,
+            {
+                product: mugProduct,
+                variants: mugVariants,
+            },
+        ],
+    ]);
+
+    return productAndVariants;
+}

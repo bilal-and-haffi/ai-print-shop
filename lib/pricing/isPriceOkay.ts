14fab0686472a7843e53fcc10433c914fe4c5048
bilalm354 <bilalm354@gmail.com>
2024-07-23 00:18:09 +0100

    finished US pricing I think

--- lib/pricing/isPriceOkay.ts
+++ lib/pricing/isPriceOkay.ts
@@ -1,55 +1,66 @@
 "use server";
 import { ProductVariant } from "@/interfaces/PrintifyTypes";
-import { convertUSDToGBP } from "@/lib/currency/convertUSDToGBP";
-import { getUkShippingCostInCents } from "../printify/shipping/getUkShippingCostsInCents";
+import { getShippingCostInCents } from "../printify/shipping/getShippingCostsInCents";
+import { UK_VAT_MULTIPLIER } from "@/app/data/consts";
+import { convertGBPToUSD } from "../currency/convertGBPToUSD";
+import { CountryCode } from "../stripe/createCheckoutSession";
 
-export async function isPriceOkay({
+export type Currency = "gbp" | "usd";
+
+export async function isSellingPriceProfitable({
     selectedVariant,
-    priceInGbp,
+    priceInLocalCurrency,
     print_provider_id,
     blueprint_id,
+    country,
 }: {
     selectedVariant: ProductVariant;
-    priceInGbp: number;
+    priceInLocalCurrency: number;
     print_provider_id: number;
     blueprint_id: number;
+    country: CountryCode;
 }) {
     const printifyProductCostInUsd = selectedVariant.cost / 100;
-    const printifyShippingCostToUkInUsd =
-        (await getUkShippingCostInCents({
+
+    const printifyShippingCostInUsd =
+        (await getShippingCostInCents({
             print_provider_id,
             blueprint_id,
+            deliveryCountry: country,
         })) / 100;
-    const VATMultiplier = 1.2;
+
     const totalPrintifyCostInUsd =
-        (printifyProductCostInUsd + printifyShippingCostToUkInUsd) *
-        VATMultiplier;
-    const totalPrintifyCostInGbp = await convertUSDToGBP(
-        totalPrintifyCostInUsd,
-    );
-    const minimumProfitInGbp = 2;
-    const profitInGbp = priceInGbp - totalPrintifyCostInGbp;
-    const isPriceOkay = profitInGbp > minimumProfitInGbp;
+        (printifyProductCostInUsd + printifyShippingCostInUsd) *
+        UK_VAT_MULTIPLIER; // does not apply in US but used anyway for safety
+
+    const minimumProfitInUsd = 5;
+
+    const priceInUsd =
+        country === "GB"
+            ? await convertGBPToUSD(priceInLocalCurrency)
+            : priceInLocalCurrency;
+
+    const profitInUsd = priceInUsd - totalPrintifyCostInUsd;
+
+    const isPriceOkay = profitInUsd > minimumProfitInUsd;
     console.log({
         printifyProductCostInUsd,
-        printifyShippingCostToUkInUsd,
-        VATMultiplier,
+        printifyShippingCostToUkInUsd: printifyShippingCostInUsd,
+        UK_VAT_MULTIPLIER,
         totalPrintifyCostInUsd,
-        totalPrintifyCostInGbp,
-        minimumProfitInGbp,
-        profitInGbp,
+        minimumProfitInUsd,
+        profitInUsd,
         isPriceOkay,
     });
 
     if (!isPriceOkay) {
         console.error({
             printifyProductCostInUsd,
-            printifyShippingCostToUkInUsd,
-            VATMultiplier,
+            printifyShippingCostToUkInUsd: printifyShippingCostInUsd,
+            UK_VAT_MULTIPLIER,
             totalPrintifyCostInUsd,
-            totalPrintifyCostInGbp,
-            minimumProfitInGbp,
-            profitInGbp,
+            minimumProfitInUsd,
+            profitInGbp: profitInUsd,
             isPriceOkay,
             msg: "Pricing issue!",
         });

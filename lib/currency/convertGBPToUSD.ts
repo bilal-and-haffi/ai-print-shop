270c56c54631a92c209d44ad7be5bba5fe63e2db
bilalm354 <bilalm354@gmail.com>
2024-07-22 22:27:38 +0100

    add force-cache to convertGBPToUSD request

--- lib/currency/convertGBPToUSD.ts
+++ lib/currency/convertGBPToUSD.ts
@@ -3,7 +3,7 @@ import { envServer } from "@/lib/env/server";
 
 export async function convertGBPToUSD(gbp: number) {
     const endpoint = `https://api.freecurrencyapi.com/v1/latest?apikey=${envServer.FREE_CURRENCY_API_KEY}&currencies=USD&base_currency=GBP`;
-    const response = await fetch(endpoint);
+    const response = await fetch(endpoint, { cache: "force-cache" });
     const data = await response.json();
     const { USD } = data.data;
     const usd = gbp * USD;

d291034d3b56ffce908e3c8f543998b123ae00e0
bilalm354 <bilalm354@gmail.com>
2024-07-23 08:09:33 +0100

    add pixel art as style

--- tests/pages/create.spec.ts
+++ tests/pages/create.spec.ts
@@ -1,18 +1,30 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
     await page.goto("/create");
+    await page.getByPlaceholder("Example: An astronaut playing").click();
+    await page.getByPlaceholder("Example: An astronaut playing").fill("bird");
 });
 
 test("User can select None for location and style and this should mean that the prompt is not concatenated", async ({
     page,
 }) => {
-    await page.getByPlaceholder("Example: An astronaut playing").click();
-    await page.getByPlaceholder("Example: An astronaut playing").fill("bird");
     await page.locator("button").filter({ hasText: "Style: None" }).click();
     await page.getByLabel("Style: None").click();
     await page.locator("button").filter({ hasText: "Location: None" }).click();
     await page.getByLabel("Location: None").click();
     await page.getByTestId("Generate Image Button").click();
     await page.waitForURL(/product/);
+});
+
+test("User can select pixel art for style and generate an image", async ({
+    page,
+}) => {
+    await page.locator("button").filter({ hasText: "Style: None" }).click();
+    await page.getByLabel("Style: Pixel Art").click();
+    await page.locator("button").filter({ hasText: "Location: None" }).click();
+    await page.getByLabel("Location: None").click();
+    await page.getByTestId("Generate Image Button").click();
+    await page.waitForURL(/product/);
+    await expect(page.locator("img").first()).toBeVisible();
 });

cf32012ff1d3c2a7f8ea6c23ccc814892e466ffd
bilalm354 <bilalm354@gmail.com>
2024-07-25 19:23:42 +0100

    extract examples to examples section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ExampleCard } from "@/components/ExamplesCard";
 import { CreateLink } from "@/components/CreateLink";
+import { ExamplesSection } from "@/components/sections/ExamplesSection";
 
 export default function Home() {
     return (
@@ -18,53 +19,7 @@ export default function Home() {
 
                 <CreateLink />
             </div>
-
-            <div className="flex flex-col gap-8 md:flex-row">
-                {examples.map(
-                    ({
-                        productType,
-                        prompt,
-                        imageSrc,
-                        imageAlt,
-                        printifyImageId,
-                    }) => (
-                        <ExampleCard
-                            productType={productType}
-                            prompt={prompt}
-                            imageSrc={imageSrc}
-                            imageAlt={imageAlt}
-                            key={imageSrc}
-                            printifyImageId={printifyImageId}
-                        />
-                    ),
-                )}
-            </div>
+            <ExamplesSection />
         </div>
     );
 }
-
-const examples: {
-    productType: string;
-    prompt: string;
-    imageSrc: string;
-    imageAlt: string;
-    printifyImageId: string;
-}[] = [
-    {
-        productType: "T Shirts",
-        prompt: "A ninja making lemonade outdoors, in the style of anime.",
-        imageSrc:
-            "in-this-style-anime-in-this-location-outdoors-a-ninja-making-lemonade.jpg",
-        imageAlt:
-            "A T shirt with a printed image of: In this style: Anime. In this location: Outdoors. A ninja making lemonade",
-        printifyImageId: "66981c740068169fdbc86e96",
-    },
-    {
-        productType: "Hoodies",
-        prompt: "An astronaut playing on an old arcade game in space",
-        imageSrc: "astronaut-playing-arcade-hoodie.jpeg",
-        imageAlt:
-            "A hoodie with a printed image of: An astronaut playing on an old arcade game in space.",
-        printifyImageId: "6697fd3381c433a6e00279b3",
-    },
-];

9eba819992f7536f05bcfb0b0421218d743509a3
bilalm354 <bilalm354@gmail.com>
2024-07-27 17:11:00 +0100

    fix options reseeting

--- components/form/SelectFormField.tsx
+++ components/form/SelectFormField.tsx
@@ -14,28 +14,31 @@ export function SelectFormField({
     setFieldValue,
     options,
     name,
+    value,
 }: {
     form: any;
     setFieldValue: Dispatch<SetStateAction<string>>;
     options: any[];
     name: string;
+    value: string;
 }) {
     return (
         <FormField
             control={form.control}
             name={name}
-            render={({ field }) => (
+            render={({}) => (
                 <FormItem className="w-full">
                     <Select
                         onValueChange={(value) => {
                             setFieldValue(value);
                         }}
-                        defaultValue={field.value}
+                        defaultValue={value}
                     >
                         <FormControl>
                             <SelectTrigger>
                                 <SelectValue
                                     placeholder={`Choose a ${name.toLowerCase()}`}
+                                    defaultValue={value}
                                 />
                             </SelectTrigger>
                         </FormControl>

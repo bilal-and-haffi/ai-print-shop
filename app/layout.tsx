578e5977cebf1f2281ad5187b49da9925f36241a
bilalm354 <bilalm354@gmail.com>
2024-07-28 03:07:42 +0100

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { CardFooter } from "@/components/ui/card";
@@ -50,6 +51,7 @@ export default function RootLayout({
                         disableTransitionOnChange
                     >
                         {children}
+                        <Analytics />
                     </ThemeProvider>
                 </main>
                 <footer className="mt-auto w-full">

121eed75d2404a4c8f3732d95fc96e870c2e5b8f
bilalm354 <bilalm354@gmail.com>
2024-07-29 00:40:53 +0100

    put on card

--- app/error.tsx
+++ app/error.tsx
@@ -1,20 +1,23 @@
 "use client";
 
+import { Card, CardContent } from "@/components/ui/card";
 import Link from "next/link";
 
 export default function ErrorPage() {
     return (
-        <div className="text-center">
-            <p>Sorry, something went wrong.</p>
-            <br />
-            <p>Please go back and try again.</p>
-            <br />
-            <p>
-                Contact us at:{" "}
-                <Link href="mailto:customer-service@ai-print-shop.com">
-                    customer-service@ai-print-shop.com
-                </Link>
-            </p>
-        </div>
+        <Card>
+            <CardContent>
+                <p>Sorry, something went wrong.</p>
+                <br />
+                <p>Please go back and try again.</p>
+                <br />
+                <p>
+                    Contact us at:{" "}
+                    <Link href="mailto:customer-service@ai-print-shop.com">
+                        customer-service@ai-print-shop.com
+                    </Link>
+                </p>
+            </CardContent>
+        </Card>
     );
 }

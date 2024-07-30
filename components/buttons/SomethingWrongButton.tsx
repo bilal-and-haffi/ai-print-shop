84393b932cb3c1afdec60cfac21220c4efe10561
bilalm354 <bilalm354@gmail.com>
2024-07-30 01:31:06 +0100

    move options into dialog after generate image

--- components/buttons/SomethingWrongButton.tsx
+++ components/buttons/SomethingWrongButton.tsx
@@ -4,7 +4,7 @@ import { Button } from "../ui/button";
 export function SomethingWrongButton() {
     return (
         <Link href="/support" className="w-full">
-            <Button variant={"outline"} className="w-full">
+            <Button variant={"secondary"} className="w-full">
                 Something Wrong?
             </Button>
         </Link>

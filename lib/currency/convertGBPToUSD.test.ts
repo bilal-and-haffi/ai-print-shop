ba344380c1415c338042b0b59891a149f738ce3d
bilalm354 <bilalm354@gmail.com>
2024-08-02 02:13:13 +0100

    changes to current conversion functions to use the API less

--- lib/currency/convertGBPToUSD.test.ts
+++ lib/currency/convertGBPToUSD.test.ts
@@ -1,7 +1,7 @@
 import { convertGBPToUSD } from "./convertGBPToUSD";
 
 describe("convertGBPToUSD", () => {
-    it("should convert 10 pounds to around 13 dollars", async () => {
+    it.skip("should convert 10 pounds to around 13 dollars", async () => {
         expect(await convertGBPToUSD(10)).toBeGreaterThan(11);
         expect(await convertGBPToUSD(10)).toBeLessThan(15);
     });
--- lib/currency/convertUSDToGBP.ts
+++ lib/currency/convertUSDToGBP.ts
@@ -2,6 +2,9 @@
 import { envServer } from "@/lib/env/server";
 
 export async function convertUSDToGBP(usd: number) {
+    if (envServer.CI || envServer.VERCEL_ENV !== "production") {
+        return 0.79; // added for monthly rate limit
+    }
     const endpoint = `https://api.freecurrencyapi.com/v1/latest?apikey=${envServer.FREE_CURRENCY_API_KEY}&currencies=USD&base_currency=GBP`;
     const response = await fetch(endpoint, {
         cache: "force-cache",

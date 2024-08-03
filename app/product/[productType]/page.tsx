bd209b6c2d1da5a147ff264b588dc70cba225b63
bilalm354 <bilalm354@gmail.com>
2024-08-04 00:32:18 +0100

    mostly working with dynamic options

--- app/product/[productType]/page.tsx
+++ app/product/[productType]/page.tsx
@@ -76,8 +76,6 @@ export default async function ProductTypePage({
     return (
         <ProductDetails
             retrievedProduct={product}
-            initialSize={getInitialSize(displayName)}
-            initialColor={getInitialColour(displayName)}
             variants={variants}
             printifyImageId={imageId}
         />

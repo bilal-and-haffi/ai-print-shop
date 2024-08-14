677ef6214a592aa5710e0515d5ca0950c0989037
bilalm354 <bilalm354@gmail.com>
2024-08-14 03:02:21 +0100

    add facebook to next config image patterns

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
                 protocol: "https",
                 hostname: "images-api.printify.com",
             },
+            {
+                protocol: "https",
+                hostname: "www.facebook.com",
+            },
         ],
     },
 };

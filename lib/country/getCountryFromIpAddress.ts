79c1746a6c53d141046f89f0a570ad3e92022f90
bilalm354 <bilalm354@gmail.com>
2024-08-14 03:23:33 +0100

    remove client logging in get coutnry from ip

--- lib/country/getCountryFromIpAddress.ts
+++ lib/country/getCountryFromIpAddress.ts
@@ -2,12 +2,10 @@
 // must be run on client because otherwise would be giving the server ip
 export async function getCountryFromIpAddress() {
     try {
-        console.log("Fetching country");
         const x = await fetch("https://ipapi.co/json/", {
             cache: "force-cache",
         });
         const { country } = await x.json();
-        console.log({ msg: "Received country", country });
         return country;
     } catch (error) {
         console.error({ error, msg: "Failed to get country so returning GB" });

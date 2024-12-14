5428de094513953050361929a2a373cbe19fa55f
bilalm354 <bilalm354@gmail.com>
2024-12-14 23:50:40 +0000

    set up stuff

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,8 @@ module.exports = {
             name: "ai-print-shop",
             script: "npm run start -- --port 3001",
             watch: ".",
+            exec_mode: "cluster",
+            instances: "max",
         },
     ],
 
@@ -17,15 +19,16 @@ module.exports = {
 
     deploy: {
         production: {
-            user: "SSH_USERNAME",
-            host: "SSH_HOSTMACHINE",
-            ref: "origin/master",
-            repo: "GIT_REPOSITORY",
-            path: "DESTINATION_PATH",
-            "pre-deploy-local": "",
+            user: "bilal",
+            host: "bilal-home-assistant.duckdns.org",
+            port: "6969",
+            ref: "origin/main",
+            repo: "git@github.com:bilal-and-haffi/ai-print-shop.git",
+            path: "/home/bilal/pm2",
+            // "pre-deploy-local": "",
             "post-deploy":
-                "npm install && pm2 reload ecosystem.config.js --env production",
-            "pre-setup": "",
+                "npm install && npm build && pm2 reload ecosystem.config.js --env production", // TODO: add db push , get prod envs etc
+            // "pre-setup": "",
         },
     },
 };

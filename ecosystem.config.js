d630fcad48e69d4189e16c02d26400663c972fd3
bilalm354 <bilalm354@gmail.com>
2024-12-15 00:21:19 +0000

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,7 @@ module.exports = {
     env_production: {
         NODE_ENV: "production",
         VERCEL_ENV: "production",
+        NVM_DIR: "~/.nvm",
     },
     env_development: {
         NODE_ENV: "development",

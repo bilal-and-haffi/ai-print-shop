b71d9dfb9334b23ca445208f1f74dda0d8c52839
bilalm354 <bilalm354@gmail.com>
2024-12-15 21:19:48 +0000

    update ecosystem config with script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
     apps: [
         {
             name: "ai-print-shop",
-            script: "npm run start -- --port 3001",
+            script: "npm",
+            args: "run start -- --port 3001",
             watch: ".",
             instances: "max",
             interpreter: "node@22.12.0",
@@ -12,7 +13,6 @@ module.exports = {
     env_production: {
         NODE_ENV: "production",
         VERCEL_ENV: "production",
-        NVM_DIR: "/home/bilal/.nvm",
     },
     env_development: {
         NODE_ENV: "development",
